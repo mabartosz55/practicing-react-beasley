a094f2d1fe13ad72eae77d692239bbc8edc965c3
michael bartosz <michaelbartosz@michaels-iMac.local>
2020-08-13 20:09:53 -0400

    initial commit

---
+++ src/components/attendee-list/AttendeeList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AttendeeList.css"
+
+    function AttendeeList (props) {
+        return (
+            <ul className = "AttendeeList">
+          
+            </ul>
+        )
+    }
+export default AttendeeList
